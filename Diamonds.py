af1aef86e9846798d0b133e714d83eca40d32bc1
emunac <emunac@gmail.com>
2021-11-10 12:03:56 +0200

    Linear model

--- Diamonds.py
+++ Diamonds.py
@@ -1,9 +1,12 @@
-import numpy as np
 import pandas as pd
 import torch
+from torch import nn as nn
+import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
 from torch.utils.data.dataset import random_split
 from torch.nn import functional
+from tqdm import tqdm
+
 
 #Learning the syntax
 
@@ -42,7 +45,13 @@ class CustomDataset(Dataset):
 
         self.features = torch.cat((torch.tensor(df[["carat"]].values), one_hot_cut, one_hot_color, one_hot_clarity,
                                    torch.tensor(df[["depth", "table", "x", "y", "z"]].values)), 1)
-        self.price = torch.tensor(df.price)
+
+        price = torch.tensor(df.price).float()
+        #standart dev
+        # mean = price.mean()
+        # std = price.std()
+        # self.price = (price - mean)/std
+        self.price = price.log()
 
     def __len__(self):
         return len(self.price)
@@ -59,8 +68,42 @@ train_size = int(0.8 * len(dataset))
 val_size = len(dataset) - train_size
 train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
 
-train_loader = DataLoader(dataset=train_dataset, batch_size=16)
-val_loader = DataLoader(dataset=val_dataset, batch_size=16)
+train_loader = DataLoader(dataset=train_dataset, batch_size=128)
+val_loader = DataLoader(dataset=val_dataset, batch_size=20)
 
+model = nn.Linear(26, 1)
+print(model.state_dict())
 
+lr = 0.0001
+n_epochs = 100
 
+loss_fn = nn.MSELoss()
+
+optimizer = optim.SGD(model.parameters(), lr=lr)
+
+for epoch in range(n_epochs):
+    model.train()
+    losses = []
+    for feat_batch, price_batch in tqdm(train_loader):
+
+        yhat:torch.Tensor = model(feat_batch.float())
+
+        loss = loss_fn(price_batch.float(), yhat.squeeze())
+        loss:torch.Tensor
+        #print(loss.item())
+        losses.append(loss.detach())
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+    model.eval()
+    val_losses = []
+    with torch.no_grad():
+        for feat_val, price_val in val_loader:
+            yhat_val: torch.Tensor = model(feat_val.float())
+            val_loss = loss_fn(price_val.float(), yhat_val.squeeze())
+            val_losses.append(val_loss.detach())
+
+    print(epoch, "loss_train:", torch.stack(losses).mean(), "loss_val:", torch.stack(val_losses).mean())
+
+print(model.state_dict())
